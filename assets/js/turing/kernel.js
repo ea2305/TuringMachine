0a0b5876a542241210d6296ae89d8e77f0906df4
laft-flyzx <laft.flyzx@gmail.com>
2016-04-13 19:47:59 -0500

    reading data
    leen los datos

--- assets/js/turing/kernel.js
+++ assets/js/turing/kernel.js
@@ -6,4 +6,39 @@ class kernel {
     }
     return newArray;
   }
+  static getQ(_Q){
+    console.log("_Q: " + _Q);
+  }
+  static getGamma(_Gamma){
+    console.log("_Gamma: " + _Gamma);
+  }
+  static getB(b){
+    console.log("b: " + b);
+  }
+  static getSigma(_Sigma){
+    console.log("_Sigma: " + _Sigma);
+  }
+  static getDelta(delta){
+    console.log("delta: " + delta);
+  }
+  static getQ0(q0){
+    console.log("q0: " + q0);
+  }
+  static getF(_F){
+    console.log("_F: " + _F);
+  }
 }
+
+$(document).ready(function() {
+
+  $('#start').click(function(){
+    kernel.getQ($('input:text[name=_Q]').val());
+    kernel.getGamma($('input:text[name=_T]').val());
+    kernel.getB($('input:text[name=B]').val());
+    kernel.getSigma($('input:text[name=_E]').val());
+    kernel.getDelta($('textarea[name=delta]').val());
+    kernel.getQ0($('input:text[name=q0]').val());
+    kernel.getF($('input:text[name=_F]').val());
+  });
+
+});
